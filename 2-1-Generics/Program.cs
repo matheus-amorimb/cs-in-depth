7e110739303d16d905090f9cfec5accd1acda07c
Matheus Batista <mbatista.sarti@gmail.com>
2025-01-08 11:14:54 -0300

    feat(chapter 1): overview

---
+++ 2-1-Generics/Program.cs
@@ -0,0 +1,21 @@
+﻿static List<string> GenerateNames()
+{
+    List<string> names = [];
+    names.Add("Joe");
+    names.Add("Matthew");
+    names.Add("Bruna");
+    return names;
+}
+
+static void PrintNames(List<string> names)
+{
+    foreach (string name in names) 
+    {
+        Console.WriteLine(name);
+    }
+}
+
+var names = GenerateNames();
+PrintNames(names);
+
+
